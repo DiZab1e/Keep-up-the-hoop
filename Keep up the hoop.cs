55835f415bf74bf01edc6835c8d63c8edac26b10
Григорий <fash1on.pro@yandex.ru>
2022-11-07 10:45:07 +0300

    mk upds

---
+++ Keep up the hoop.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Kata
+{
+    public static string HoopCount(int n)
+    {
+        if (n < 10)
+            return $"Keep at it until you get it";
+        else
+            return $"Great, now move on to tricks";
+    }
+}
